1f5f00586465630112325b375896cac919785ddc
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-04-16 19:53:55 +0530

    15042023

---
+++ index.js
@@ -0,0 +1,29 @@
+var nameInput = document.getElementById("name");
+var professionInput = document.getElementById("profession");
+var ageInput = document.getElementById("age");
+var button =document.getElementById("adduser");
+button.addEventListener("click", () => {
+    var name = nameInput.value;
+    var profession = professionInput.value;
+    var age = ageInput.value;
+    if (name == "" || profession == "" || age == "") {
+        document.querySelector(".status-msg").style.display = "block";
+        document.querySelector(".status-msg").style.display = "red";
+    } else {
+        document.querySelector("emp-li-container").innerHTML +=
+        `<div class="emp-card" id="id-${name}-${profession}-${age}">
+            <div class="details">
+                <span>Name: ${name}</span>
+                <span>Profession: ${profession}</span>
+                <span>Age: ${age}</span>
+            </div>
+            <button id="del-user-btn" onclick="deleteUser('id-${name}-${profession}-${age}')">Delete User</button>
+        </div>`;
+    }
+});
+function deleteUser(id) {
+    var userToDel = document.getElementById(id);
+    console.log(userToDel);
+    document.querySelector(".emp-li-container").removeChild(userToDel);
+    alert("User deleted");
+}
